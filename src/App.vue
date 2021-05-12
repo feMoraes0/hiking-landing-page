e9a6e2f552b76066bd90635d0f6c4f31d2e148d8
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-05-11 21:12:22 -0300

    feat: add base header component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="app">
-    <p>Hello world</p>
+    <Header/>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header.vue';
+
 export default {
   name: 'App',
+  components: {
+    Header,
+  },
 };
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
+  align-items: flex-start;
+  background-color: #0B1D26;
   display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  min-height: 100vh;
+  width: 100vw;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <img src="@/assets/images/logo.png" alt="logo">
+    <ul>
+      <li><a href="/">Equipament</a></li>
+      <li><a href="/">About us</a></li>
+      <li><a href="/">Blog</a></li>
+    </ul>
+    <button>
+      <span>Account</span>
+    </button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+}
+</style>
