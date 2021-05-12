99c048737e5c3cff12b2fd6d5ace013ae3b25465
Fernando de Moraes Lopes <fernando.mlopes@telefonica.com>
2021-05-11 22:32:23 -0300

    feat: add header component with font files

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,14 @@ export default {
 
 <style lang="scss">
 * {
+  box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
 
 #app {
   align-items: flex-start;
-  background-color: #0B1D26;
+  background-color: $background-color;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <header>
     <img src="@/assets/images/logo.png" alt="logo">
     <ul>
-      <li><a href="/">Equipament</a></li>
+      <li><a href="/">Equipment</a></li>
       <li><a href="/">About us</a></li>
       <li><a href="/">Blog</a></li>
     </ul>
@@ -24,6 +24,41 @@ header {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  padding: 5.33vh 4.167vw;
   width: 100%;
+
+  ul {
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    list-style: none;
+
+    li {
+      margin-right: 2.083vw;
+
+      &:last-child {
+        margin-right: 0;
+      }
+
+      a {
+        color: $white-color;
+        font-family: $gilroy-bold;
+        font-size: 1.125rem;
+        line-height: 1.375rem;
+        text-decoration: none;
+      }
+    }
+  }
+
+  button {
+    background-color: transparent;
+    border: none;
+    color: $white-color;
+    cursor: pointer;
+    font-family: $gilroy-normal;
+    font-size: 1.0625rem;
+    line-height: 1.3125rem;
+  }
 }
 </style>
